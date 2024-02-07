65e5994802e68e4039b979d346206c4775a3448f
Евгений Литвинов <leo-jek@yandex.ru>
2024-02-08 01:32:58 +0500

    Homework 01. Task 2. Fix 7.

--- Cloud infrastructure/01/src_2/docker.tf
+++ Cloud infrastructure/01/src_2/docker.tf
@@ -3,29 +3,21 @@ provider "docker" {
 }
 
 resource "docker_image" "mysql" {
-  name = "mysql:8"
+  name         = "mysql:8"
+  keep_locally = true
 }
 
 resource "docker_container" "mysql" {
   image = docker_image.mysql.image_id
   name  = "mysql"
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello!'",
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "root"
-      host        = self.network_data.0.ip_address
-      timeout     = "2m"
-    }
-  }
-
   ports {
-    internal = 3306
+    internal = 22
+    external = 3306
+    ip = "127.0.0.1"
   }
+  # Не удалось разобраться, как подключиться по ssh к контейнеру.connection {
+  # 127.0.0.1:3306 доступен, но ssh -p 3306 127.0.0.1 не заходит. 
 
   env = [
     "MYSQL_ROOT_PASSWORD=example_${random_password.random_string.result}",
