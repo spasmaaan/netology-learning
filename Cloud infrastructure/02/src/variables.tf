0ea6021f77df744ae575a469e4fa42319cc011fd
Евгений Литвинов <leo-jek@yandex.ru>
2024-02-12 23:13:29 +0500

    CI. Homework 02. Tasks 1-3.

--- Cloud infrastructure/02/src/variables.tf
+++ Cloud infrastructure/02/src/variables.tf
@@ -19,6 +19,10 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+variable "db_zone" {
+  type        = string
+  default     = "ru-central1-b"
+}
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
@@ -38,4 +42,75 @@ variable "vms_ssh_root_key" {
   type        = string
   default     = "<your_ssh_ed25519_key>"
   description = "ssh-keygen -t ed25519"
+}
+
+variable "yc_keyfile" {
+  type        = string
+  default     = "~/.authorized_key.json"
+  description = "YC service account key file"
+}
+variable "image_name" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "Ubuntu image name"
+}
+
+variable "vm_web_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "VM web name"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  default     = "standard-v1"
+  description = "VM web platform id"
+}
+
+variable "vm_web_cores" {
+  type        = number
+  default     = 2
+  description = "VM web cores count"
+}
+
+variable "vm_web_memory" {
+  type        = number
+  default     = 1
+  description = "VM web memory GBs count"
+}
+
+variable "vm_web_core_fraction" {
+  type        = number
+  default     = 5
+  description = "VM web core fraction"
+}
+
+variable "vm_db_name" {
+  type        = string
+  default     = "netology-develop-platform-db"
+  description = "VM db name"
+}
+
+variable "vm_db_platform_id" {
+  type        = string
+  default     = "standard-v1"
+  description = "VM db platform id"
+}
+
+variable "vm_db_cores" {
+  type        = number
+  default     = 2
+  description = "VM db cores count"
+}
+
+variable "vm_db_memory" {
+  type        = number
+  default     = 2
+  description = "VM db memory GBs count"
+}
+
+variable "vm_db_core_fraction" {
+  type        = number
+  default     = 20
+  description = "VM db core fraction"
 }
---
+++ Cloud infrastructure/02/src/vms_platform.tf
@@ -0,0 +1,57 @@
+resource "yandex_compute_instance" "platform-web" {
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
+  zone        = var.default_zone
+
+  resources {
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = true
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+}
+
+resource "yandex_compute_instance" "platform-db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  zone        = var.db_zone
+  
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = true
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+}
