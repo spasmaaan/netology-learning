d7701880aee343b00baf7f5748b5aa7b7f6f97eb
Евгений Литвинов <leo-jek@yandex.ru>
2024-02-18 21:52:32 +0500

    Cloud. Homework 03. Task 6.

--- Cloud infrastructure/03/src/ansible.tf
+++ Cloud infrastructure/03/src/ansible.tf
@@ -1,10 +1,30 @@
 resource "local_file" "hosts_templatefile" {
   content = templatefile("${path.module}/hosts.tftpl", { 
-    webservers = yandex_compute_instance.web,
-    dbservers  = yandex_compute_instance.db,
-    storages   = tolist([yandex_compute_instance.storage])
+    webservers = {
+      for key, web_vm in yandex_compute_instance.web:
+        key => {
+          name = web_vm.name,
+          ip = lookup(web_vm.network_interface[0].nat_ip_address, web_vm.network_interface[0].ip_address), 
+          fqdn = web_vm.fqdn
+        }
+    },
+    dbservers  = {
+      for key, db_vm in yandex_compute_instance.db:
+        key => {
+          name = db_vm.name,
+          ip = lookup(db_vm.network_interface[0].nat_ip_address, db_vm.network_interface[0].ip_address), 
+          fqdn = db_vm.fqdn
+        }
+    },
+    storages   = {
+      for key, st_vm in tolist([yandex_compute_instance.storage]):
+        key => {
+          name = st_vm.name,
+          ip = lookup(st_vm.network_interface[0].nat_ip_address, st_vm.network_interface[0].ip_address), 
+          fqdn = st_vm.fqdn
+        }
+    }
   })
-
   filename = "${abspath(path.module)}/${local.ansible_hosts_cfg_filename}"
 }
 
