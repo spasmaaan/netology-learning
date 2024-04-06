068b2b8707b8408f262f379e94007da4e24321fa
Евгений Литвинов <leo-jek@yandex.ru>
2024-04-07 02:04:19 +0500

    Cloud. HW04. Task 7. Fix 3.

--- Cloud infrastructure/04/src/main.tf
+++ Cloud infrastructure/04/src/main.tf
@@ -105,15 +105,15 @@ data "vault_generic_secret" "vault_example" {
   path = "secret/example"
 }
 
-resource "vault_mount" "secret" {
-  path        = "secret"
+resource "vault_mount" "secret2" {
+  path        = "secret2"
   type        = "kv"
   options     = { version = "1" }
   description = "KV Version 1 secret engine mount"
 }
 
-resource "vault_kv_secret" "secret" {
-  path = "${vault_mount.secret.path}/example"
+resource "vault_kv_secret" "example" {
+  path = "${vault_mount.secret2.path}/example"
   data_json = jsonencode({
     test2   = "congrats!2"
   })
--- Cloud infrastructure/04/src/outputs.tf
+++ Cloud infrastructure/04/src/outputs.tf
@@ -8,4 +8,8 @@ output "vpc_dev_list" {
 
 output "vault_example" {
  value = "${nonsensitive(data.vault_generic_secret.vault_example.data)}"
+}
+
+output "vault_example2" {
+ value = "${nonsensitive(vault_kv_secret.example.data)}"
 } 
