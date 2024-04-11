c80c44af239546ffc59e1600dec4248fba63585e
Евгений Литвинов <leo-jek@yandex.ru>
2024-04-12 03:51:55 +0500

    Cloud. HW05. Task 4. Task 5.

--- Cloud infrastructure/05/terraform-05/vpc_root/variables.tf
+++ Cloud infrastructure/05/terraform-05/vpc_root/variables.tf
@@ -25,3 +25,50 @@ variable "default_cidr" {
   default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
+
+variable "task_4_1" {
+  type        = string 
+  description = "IP адрес"
+  validation {
+    condition = cidrhost(var.task_4_1)
+    error_message = "Переменная содержит неправильный IP адрес."
+  }
+}
+
+variable "task_4_2" {
+  type        = set(string) 
+  description = "Список IP адресов"
+  validation {
+    condition = alltrue([
+      for ip in var.task_4_2 : cidrhost(ip)
+    ])
+    error_message = "Список содержит хотя бы один неправильный IP адрес."
+  }
+}
+
+variable "task_5_1" {
+  type        = string 
+  description = "любая строка"
+  validation {
+    condition = can(regex("^[a-z ]+$", var.task_5_1))
+    error_message = "Строка содержит символы верхнего регистра или недопустимые символы."
+  }
+}
+
+variable "in_the_end_there_can_be_only_one" {
+    description="Who is better Connor or Duncan?"
+    type = object({
+        Dunkan = optional(bool)
+        Connor = optional(bool)
+    })
+
+    default = {
+        Dunkan = true
+        Connor = false
+    }
+
+    validation {
+        error_message = "There can be only one MacLeod"
+        condition = tobool(var.in_the_end_there_can_be_only_one.Duncan) != tobool(var.in_the_end_there_can_be_only_one.Duncan)
+    }
+}
