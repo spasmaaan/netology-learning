9650c2f4e8d8e3de7c36520576445a263edf6748
Евгений Литвинов <leo-jek@yandex.ru>
2024-04-30 20:00:23 +0500

    Ansible. HW01. Task a5. Fix 2.

--- Ansible/01/src/manage.sh
+++ Ansible/01/src/manage.sh
@@ -3,21 +3,25 @@
 case $1 in
     up)
         echo "Enter password: "
-        read ansible_vault_password
+        read -s ansible_vault_password
 
+        echo ""
         echo "=================="
         echo "  Starting hosts  "
         echo "------------------"
+        echo ""
 
         docker compose up -d
         
+        echo ""
         echo "=================="
         echo "  Hosts started   "
         echo "------------------"
+        echo ""
 
-        echo ${ansible_vault_password} | ansible-playbook site.yml -i inventory/prod.yml --vault-password-file=/bin/cat
+        echo ${ansible_vault_password} | ansible-playbook playbook/site.yml -i playbook/inventory/prod.yml --vault-password-file=/bin/cat
         
-        
+        echo ""
         echo "=================="
         echo "  Hosts prepared  "
         echo "------------------"
@@ -27,15 +31,19 @@ case $1 in
         echo "=================="
         echo "  Stopping hosts  "
         echo "------------------"
+        echo ""
 
         docker compose down
         
+        echo ""
         echo "=================="
         echo "  Hosts stopped   "
         echo "------------------"
+        echo ""
     ;;
 
     *)
+        echo ""
         echo "=================="
         echo "     Skipped      "
         echo "------------------"
